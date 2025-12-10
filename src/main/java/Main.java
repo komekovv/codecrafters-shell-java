76a03f82df266e92a02c5555a2820eb582a38705
Kakamyrat Komekov <kakamyratkomekov@gmail.com>
2025-12-10 14:51:25 +0500

    repl implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         System.out.print("$ ");
-         Scanner scanner = new Scanner(System.in);
-         String command = scanner.nextLine();
+        boolean commandExist = false;
+        while (!commandExist) {
+             System.out.print("$ ");
+             Scanner scanner = new Scanner(System.in);
+             String command = scanner.nextLine();
 
-        System.out.println(command + ": command not found");
+            System.out.println(command + ": command not found");
+        }
     }
 }
