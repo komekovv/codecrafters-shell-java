fa1d1d6d983fed1a27b7b1d719e5b7707e452608
Kakamyrat Komekov <kakamyratkomekov@gmail.com>
2025-12-10 15:00:55 +0500

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        boolean commandExist = false;
-        while (!commandExist) {
-             System.out.print("$ ");
-             Scanner scanner = new Scanner(System.in);
-             String command = scanner.nextLine();
-
-            System.out.println(command + ": command not found");
-        }
+        System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        System.out.println(command + ": command not found");
+        main(new String[0]);
+        scanner.close();
     }
 }
